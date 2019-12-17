3a45938a7225f7fbd0f67e623f1020dbd4d1e9c6
chathuraedirisingh <arkxstyler.forum@gmail.com>
2019-12-18 05:14:42 +0530

    Updates

---
+++ precache-manifest.d8f48cec93d8fe12a1f355e1775e5d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c47b192639fa1b1fe5be011d97863fd",
+    "url": "/rr/index.html"
+  },
+  {
+    "revision": "f37cb830d9db13b247c8",
+    "url": "/rr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71a59fa9ce6566690ead",
+    "url": "/rr/static/js/2.0c7a1605.chunk.js"
+  },
+  {
+    "revision": "8214720c69f7d7952bbc10449ccd1948",
+    "url": "/rr/static/js/2.0c7a1605.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f37cb830d9db13b247c8",
+    "url": "/rr/static/js/main.e4b12867.chunk.js"
+  },
+  {
+    "revision": "a667005a788650f57b2f",
+    "url": "/rr/static/js/runtime-main.5915fe32.js"
+  }
+]);
